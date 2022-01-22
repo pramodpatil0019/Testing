509f836e39a3d6d76bdeb06a1caab9b314aca203
pramod <pramodpatil0019@gmail.com>
2022-01-22 16:11:46 +0530

    First commit

---
+++ src/main/java/com/example/git/Git_Testing/GitTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.git.Git_Testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/git/Git_Testing/GitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git.Git_Testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
